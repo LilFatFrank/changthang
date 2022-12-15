0e80b0572c2a75fa0738d03bef591f01a78db025
LilFatFrank <karan.parsnani@ymail.com>
2022-12-16 00:25:11 +0530

    modifying image names

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,27 +3,27 @@ export const SLIDES = [
         key: 0,
         title: 'Standard Circuit',
         subTitle: '7 days - 6 nights',
-        image: 'assets/1.jpg',
+        image: 'assets/Slide-1.jpg',
         link: 'Jump on the bandwagon'
     },
     {
         key: 1,
         title: 'Highest Road on the Planet',
         subTitle: '8 days - 7 nights',
-        image: 'assets/2.jpg',
+        image: 'assets/Slide-2.jpg',
         link: 'Conquer the formidable'
     },
     {
         key: 2,
         title: 'Raw Changthang',
         subTitle: '10 days - 9 nights',
-        image: 'assets/3.jpg',
+        image: 'assets/Slide-3.jpg',
         link: 'As raw as it gets'
     },
     {
         key: 3,
         title: 'Talk to us',
-        image: 'assets/4.jpg',
+        image: 'assets/Slide-4.jpg',
         link: 'Reach out to us'
     }
 ]
